c0203e9f3b15110491b55b145226b1018d4d54e3
Victor <victor.tineo141@gmail.com>
2021-06-22 23:03:27 -0300

    feat: break the line

--- packages/tineo-core/src/components/App.js
+++ packages/tineo-core/src/components/App.js
@@ -5,6 +5,7 @@ function App(){
   const [active, setActive] = useState(true)
   return <div>
       <Button onClick={() => setActive(!active)}>oi</Button>
+      <hr />
       {active && <ActiveConsole title="olar" />}
       <h1>Hello World From React-Rollup</h1>
     </div>
