7e5cf42d2280512fe419b5264074d6bded5bbbae
Victor <victor.tineo141@gmail.com>
2021-06-22 23:53:17 -0300

    chore: change rollup html output method

--- packages/tineo-core/rollup.config.js
+++ packages/tineo-core/rollup.config.js
@@ -1,14 +1,13 @@
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
 import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import html from '@rollup/plugin-html';
 
 export default {
   input: "src/index.js",
   output: {
-    file: "public/dist/bundle.js",
+    file: "dist/bundle.js",
     format: "iife",
     sourcemap: true,
   },
@@ -23,5 +22,21 @@ export default {
       presets: ["@babel/preset-react"],
     }),
     commonjs(),
+    html({
+      template({ attributes, bundle, files, publicPath, title }) {
+        return `
+          <html>
+            <head>
+              <meta charset="utf-8">
+              <title>Tineo Portal | Home</title>
+            </head>
+            <body>
+              <script src="${bundle}"></script>
+              <script nomodule src="s.js"></script>
+            </body>
+          </html>
+        `;
+      }
+    })
   ]
 };
